8c381387846bd5a5f1342eb04a3b5aa748406b9e
Digity Developer <developer@idigity.ru>
2011-12-16 09:55:54 +0400

    minor fixes

--- tinchy.py
+++ tinchy.py
@@ -1,9 +1,9 @@
 import bottle, random, shelve
 
 def generate_tinchy_id():
-    return ''.join(random.sample('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890', 3))
+    return ''.join( random.sample( 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890' , 3 ) )
 
-@route('/')
+@bottle.route('/')
 def index():
     return """
     <h1>Get your tinchy URL!</h1>
@@ -13,26 +13,26 @@ def index():
     </form>
     """
 
-@post('/')
+@bottle.post('/')
 def make_tinchy_url():
-    fatty_url = request.POST.get('url', '')
-    if fatty_url[:7] == "http://":
-        storage = shelve.open("tinchy")
+    fatty_url = request.POST.get( 'url' , '' )
+    if fatty_url[:7] == 'http://':
+        storage = shelve.open( 'tinchy' )
         tinchy_id = generate_tinchy_id()
-        while tinchy_id in storage:
+        while storage.has_key( tinchy_id ):
             tinchy_id = generate_tinchy_id()
         storage[tinchy_id] = fatty_url
         storage.close()
         return 'Your tinchy url is: <a href="%s">%s</a>' % ( request.url + tinchy_id )
     return 'Boo, <a href="%s">enter a proper url this time</a>.' % request.url
 
-@route('/<tinchy_id>')
-def redirect_to_fatty_url(tinchy_id):
-    storage = shelve.open("tinchy")
+@bottle.route('/<tinchy_id>')
+def redirect_to_fatty_url( tinchy_id ):
+    storage = shelve.open( 'tinchy' )
     if tinchy_id not in storage:
         storage.close()
         return "Nope, this tinchy URL wasn't found." 
     storage.close()
-    redirect(storage[tinchy_id])
+    redirect( storage[tinchy_id] )
     
-run(host='localhost', port=8080)
+bottle.run(host='0.0.0.0', port=80)
